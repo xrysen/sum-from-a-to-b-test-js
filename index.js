be8e7298e551c3af1d2afa2bded5d7b382a3d09f
Sean Oyler <xrysen@gmail.com>
2020-11-01 22:58:19 +0000

    Solved recurssion

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(fromN === toN) {
+    return toN;
+  } else {
+    return toN + sum(fromN, toN - 1);
+  }
 }
 
 module.exports = sum;
